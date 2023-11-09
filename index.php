ce11b0d2425450592e60ecd18d6ba5f2ed82040d
Rafael Almeida <rafa261010@outlook.com>
2023-11-09 10:55:35 -0300

    login_php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php 
+        if(!isset($_SESSION['login'])) {
+            if(isset($_POST['acao'])) {
+                $login = 'rafael';
+                $senha = '123456';
+
+                $loginForm = $_POST['login'];
+                $senhaForm = $_POST['senha'];
+
+                if($login == $loginForm &&  $senha == $senhaForm) {
+                    //logado com sucesso!
+                    $_SESSION['login'] = $login;
+                    header('Location: index.php');
+                } else {
+                    //Algum erro ocorreu
+                    echo 'Dados inválidos';
+                }
+            }
+
+            include('login.php');
+        } else {
+            if(isset($_GET['logout'])) {
+                unset($_SESSION['login']);
+                session_destroy();
+                header('Location: index.php');
+            }
+            include('home.php');
+        }
+    ?>
+    
+</body>
+</html>
